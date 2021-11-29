c2df86b5bba67459e00e4b73ffb607bcc32aee4f
madzhugin <madzhugin@yandex.ru>
2021-11-29 19:11:06 +0300

    добавлена типизация возвращаемого значения для inclineNum

--- src/Mav/Slovo/Grammar.php
+++ src/Mav/Slovo/Grammar.php
@@ -13,7 +13,11 @@ class Grammar {
      *
      * @return string
     */
-    public static function inclineNum (int $number, array $titles, bool $full=false)
+    public static function inclineNum (
+            int $number,
+            array $titles,
+            bool $full=false
+        ): string
     {  
         $cases = array (2, 0, 1, 1, 1, 2);
         $result = $titles[ ($number%100 > 4 && $number%100 < 20) ? 2 : $cases[min($number%10, 5)] ];
